c4741d492d4565dd05ab8db06b02d36864049df2
AnshKathpal <storage.2.3107@gmail.com>
2023-09-23 04:44:16 +0530

    mario-maze

--- maze/src/Components/Maze.jsx
+++ maze/src/Components/Maze.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import "../maze.css"
+import "../maze.css";
+import queen from "../images/queen.png"
 
 export const Maze = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -74,7 +75,7 @@ export const Maze = () => {
   }
 
   function getStepSize() {
-    return 50;
+    return 40;
   }
 
   return (
@@ -83,84 +84,99 @@ export const Maze = () => {
         width: "100%",
         height: "100vh",
         display: "flex",
-        flexDirection: "column", // To display the command below the maze
+        flexDirection: "column", 
         alignItems: "center",
+        justifyContent : "center"
       }}
     >
-      <p style={{ fontSize: "20px" }}>Recent Command: {recentCommand}</p>
+      <h1>Mario Maze</h1>
+      <p style={{ fontSize: "20px" }}>Mario Moves: {recentCommand}</p>
       <div
         style={{
           position: "relative",
           width: "30%",
           height: "50vh",
-          border: "1px solid black",
+          border: "1px solid blue",
           display: "grid",
           gridTemplateColumns: "repeat(7,1fr)",
           gridTemplateRows: "repeat(7,1fr)",
         }}
       >
-
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
-<div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div style={{display : "flex", justifyContent : "center", alignItems : "center"}} className="mazeBox">
+          <img style={{height : "50px"}} src="https://mario.wiki.gallery/images/6/65/Goomba_-_Mario_Party_Superstars.png" alt="" />
+        </div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div style={{display : "flex", justifyContent : "center", alignItems : "center", borderRight : "none"}} className="mazeBox">
+          <img style={{height : "50px"}} src="https://mario.wiki.gallery/images/6/65/Goomba_-_Mario_Party_Superstars.png" alt="" />
+        </div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div style={{display : "flex", justifyContent : "center", alignItems : "center", borderRight : "none"}} className="mazeBox">
+          <img style={{height : "50px"}} src="https://mario.wiki.gallery/images/6/65/Goomba_-_Mario_Party_Superstars.png" alt="" />
+        </div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox"></div>
+        <div className="mazeBox">
+          <img style={{ height : "65px"}} src={queen} alt="" />
+        </div>
 
         <div
           style={{
-            border: "1px solid red",
-            width: "69px",
-            height: "69px",
+            width: "50px",
+            height: "50px",
             position: "absolute",
+            // border : "1px solid red",
             top: `${position.y}px`,
             left: `${position.x}px`,
             transition: "all 0.1s ease",
+            display : "flex",
+            justifyContent:"center",
+            alignItems : "center",
+            transform: "translate(20%, 20%)",
           }}
-        ></div>
+        >
+          <img style = {{height : "100%"}} src="https://freepngimg.com/thumb/categories/1130.png" alt="" />
+        </div>
       </div>
     </div>
   );
